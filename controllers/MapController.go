fb8ef617084ce17b744d5748513273a829389157
munkh-altai <munkaltai@gmail.com>
2024-11-28 12:07:15 +0800

    legend_order added

--- controllers/MapController.go
+++ controllers/MapController.go
@@ -35,7 +35,11 @@ func GetMapLayers(c *fiber.Ctx) error {
 	// Adjust preloading to correctly apply ordering to Categories and Layers
 	result := DB.DB.Preload("Categories", func(db *gorm.DB) *gorm.DB {
 		return db.Order("category_order ASC").Where("is_active = ?", true).Preload("Layers", func(db *gorm.DB) *gorm.DB {
-			return db.Order("layer_order ASC").Where("is_active = ?", true).Preload("Legends").Order("legend_order ASC").Preload("Permissions")
+			return db.Order("layer_order ASC").Where("is_active = ?", true).
+				Preload("Legends", func(db *gorm.DB) *gorm.DB {
+					return db.Order("legend_order ASC")
+				}).
+				Preload("Permissions")
 		})
 	}).Where("id = ?", id).First(&currentMap)
 
