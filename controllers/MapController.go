6620f44890d7caacfe8b5ffa84335994bc772455
Bilguun <4bilguun@gmail.com>
2025-09-09 15:43:30 +0800

    now filter category and layers in maps API

--- controllers/MapController.go
+++ controllers/MapController.go
@@ -4,24 +4,26 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/khankhulgun/khanmap/models"
 	"github.com/khankhulgun/khanmap/sprite"
 	"github.com/lambda-platform/lambda/DB"
+	agentUtils "github.com/lambda-platform/lambda/agent/utils"
 	"github.com/lambda-platform/lambda/config"
 	"gorm.io/gorm"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 func GetMapLayers(c *fiber.Ctx) error {
-
-	// Get the 'id' parameter from the URL
 	id := c.Params("id")
 	generate := c.Query("generate")
 	secure := c.Query("secure")
+
 	if id == "" {
 		// Return a 400 Bad Request error if no ID is provided
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -37,7 +39,7 @@ func GetMapLayers(c *fiber.Ctx) error {
 	result := DB.DB.Preload("Filters").Preload("Categories", func(db *gorm.DB) *gorm.DB {
 		return db.Order("category_order ASC").Where("is_active = ?", true).
 			Preload("Layers", func(db *gorm.DB) *gorm.DB {
-				return db.Order("layer_order ASC").Where("is_active = ?", true).
+				return db.Order("layer_order ASC").Where("is_public = ? AND is_active = ?", true, true).
 					Preload("Legends", func(db *gorm.DB) *gorm.DB {
 						return db.Order("legend_order ASC")
 					}).
@@ -61,6 +63,14 @@ func GetMapLayers(c *fiber.Ctx) error {
 			"error":   result.Error,
 		})
 	}
+
+	var filteredCategories []models.ViewMapLayerCategories
+	for _, category := range currentMap.Categories {
+		if len(category.Layers) > 0 {
+			filteredCategories = append(filteredCategories, category)
+		}
+	}
+	currentMap.Categories = filteredCategories
 
 	mapStyle, generateErr := generateVectorTileStyle(currentMap.Categories, secure)
 
@@ -123,6 +133,335 @@ func GetMapLayers(c *fiber.Ctx) error {
 		}
 
 		sprite.MakeSprite(fmt.Sprintf("./public/map/%s/sprite/images", id), fmt.Sprintf("./public/map/%s/sprite/%s", id, id))
+	}
+
+	return c.JSON(currentMap)
+}
+
+func GetMapLayersWithAuthTemp(c *fiber.Ctx) error {
+	id := c.Params("id")
+	secure := c.Query("secure")
+	if id == "" {
+		// Return a 400 Bad Request error if no ID is provided
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":  "error",
+			"message": "ID parameter is required",
+		})
+	}
+	id = strings.TrimSuffix(id, ".json")
+
+	user, err := agentUtils.AuthUserObject(c)
+	if err != nil {
+		log.Printf("User not found: %v", err)
+		return c.Status(fiber.StatusUnauthorized).SendString("User not found")
+	}
+
+	roleVal, ok := user["role"]
+	roleFloat, isFloat := roleVal.(float64)
+	roleInt := int(roleFloat)
+	if !ok || !isFloat {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User role is missing or not a float",
+		})
+	}
+
+	idVal, ok := user["id"]
+	idInt64, isInt64 := idVal.(int64)
+	if !ok || !isInt64 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User id is missing or not an int64",
+		})
+	}
+
+	var currentMap models.Map
+	result := DB.DB.Preload("Filters").Preload("Categories", func(db *gorm.DB) *gorm.DB {
+		return db.Order("category_order ASC").Where("is_active = ?", true).
+			Preload("Layers", func(db *gorm.DB) *gorm.DB {
+				return db.Order("layer_order ASC").Where("is_active = ?", true).
+					Preload("Legends", func(db *gorm.DB) *gorm.DB {
+						return db.Order("legend_order ASC")
+					}).
+					Preload("AdminFilters").
+					Preload("RolePermissions").
+					Preload("UserPermissions")
+			})
+	}).Where("id = ?", id).First(&currentMap)
+
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"status":  "error",
+				"message": "Map layer not found",
+			})
+		}
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Error retrieving map layer",
+			"error":   result.Error,
+		})
+	}
+
+	var filteredCategories []models.ViewMapLayerCategories
+
+	for i := range currentMap.Categories {
+		var filteredLayers []models.MapLayers
+
+		for _, layer := range currentMap.Categories[i].Layers {
+			shouldIncludeLayer := true
+
+			if layer.IsPermission {
+				if len(layer.RolePermissions) > 0 {
+					hasPermission := false
+					roleFound := false
+
+					for _, perm := range layer.RolePermissions {
+						if roleInt == perm.RoleID {
+							roleFound = true
+							break
+						}
+					}
+
+					if layer.IsRoleException != nil && *layer.IsRoleException != 0 {
+						hasPermission = !roleFound
+					} else {
+						hasPermission = roleFound
+					}
+
+					if !hasPermission {
+						shouldIncludeLayer = false
+					}
+				}
+
+				if len(layer.UserPermissions) > 0 {
+					hasPermission := false
+					userFound := false
+
+					for _, perm := range layer.UserPermissions {
+						if idInt64 == int64(perm.UserID) {
+							userFound = true
+							break
+						}
+					}
+
+					if layer.IsRoleException != nil && *layer.IsRoleException != 0 {
+						hasPermission = !userFound
+					} else {
+						hasPermission = userFound
+					}
+
+					if !hasPermission {
+						shouldIncludeLayer = false
+					}
+				}
+			}
+
+			if shouldIncludeLayer {
+				filteredLayers = append(filteredLayers, layer)
+			}
+		}
+
+		if len(filteredLayers) > 0 {
+			currentMap.Categories[i].Layers = filteredLayers
+			filteredCategories = append(filteredCategories, currentMap.Categories[i])
+		}
+	}
+
+	currentMap.Categories = filteredCategories
+
+	for i := range currentMap.Categories {
+		for j := range currentMap.Categories[i].Layers {
+			currentMap.Categories[i].Layers[j].RolePermissions = nil
+			currentMap.Categories[i].Layers[j].UserPermissions = nil
+		}
+	}
+
+	mapStyle, generateErr := generateVectorTileStyle(currentMap.Categories, secure)
+
+	currentMap.Version = mapStyle.Version
+	currentMap.Layers = mapStyle.Layers
+	currentMap.Sources = mapStyle.Sources
+
+	spriteURL := config.LambdaConfig.Domain + "/map/" + id + "/sprite/" + id
+	hasProtocol := strings.HasPrefix(spriteURL, "http://") || strings.HasPrefix(spriteURL, "https://")
+
+	if !hasProtocol {
+		currentMap.Sprite = "https://" + spriteURL
+	} else {
+		currentMap.Sprite = spriteURL
+	}
+
+	if generateErr != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status": "error",
+			"error":  generateErr.Error(),
+		})
+	}
+
+	return c.JSON(currentMap)
+}
+
+func GetMapLayersWithAuth(c *fiber.Ctx) error {
+	id := c.Params("id")
+	secure := c.Query("secure")
+	if id == "" {
+		// Return a 400 Bad Request error if no ID is provided
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":  "error",
+			"message": "ID parameter is required",
+		})
+	}
+
+	user, err := agentUtils.AuthUserObject(c)
+	if err != nil {
+		log.Printf("User not found: %v", err)
+		return c.Status(fiber.StatusUnauthorized).SendString("User not found")
+	}
+
+	roleVal, ok := user["role"]
+	roleFloat, isFloat := roleVal.(float64)
+	roleInt := int(roleFloat)
+	if !ok || !isFloat {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User role is missing or not a float",
+		})
+	}
+
+	idVal, ok := user["id"]
+	idInt64, isInt64 := idVal.(int64)
+	if !ok || !isInt64 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User id is missing or not an int64",
+		})
+	}
+
+	var currentMap models.Map
+	result := DB.DB.Preload("Filters").Preload("Categories", func(db *gorm.DB) *gorm.DB {
+		return db.Order("category_order ASC").Where("is_active = ?", true).
+			Preload("Layers", func(db *gorm.DB) *gorm.DB {
+				return db.Order("layer_order ASC").Where("is_active = ?", true).
+					Preload("Legends", func(db *gorm.DB) *gorm.DB {
+						return db.Order("legend_order ASC")
+					}).
+					Preload("AdminFilters").
+					Preload("RolePermissions").
+					Preload("UserPermissions")
+			})
+	}).Where("id = ?", id).First(&currentMap)
+
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"status":  "error",
+				"message": "Map layer not found",
+			})
+		}
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Error retrieving map layer",
+			"error":   result.Error,
+		})
+	}
+
+	var filteredCategories []models.ViewMapLayerCategories
+
+	for i := range currentMap.Categories {
+		var filteredLayers []models.MapLayers
+
+		for _, layer := range currentMap.Categories[i].Layers {
+			shouldIncludeLayer := true
+
+			if layer.IsPermission {
+				if len(layer.RolePermissions) > 0 {
+					hasPermission := false
+					roleFound := false
+
+					for _, perm := range layer.RolePermissions {
+						if roleInt == perm.RoleID {
+							roleFound = true
+							break
+						}
+					}
+
+					if layer.IsRoleException != nil && *layer.IsRoleException != 0 {
+						hasPermission = !roleFound
+					} else {
+						hasPermission = roleFound
+					}
+
+					if !hasPermission {
+						shouldIncludeLayer = false
+					}
+				}
+
+				if len(layer.UserPermissions) > 0 {
+					hasPermission := false
+					userFound := false
+
+					for _, perm := range layer.UserPermissions {
+						if idInt64 == int64(perm.UserID) {
+							userFound = true
+							break
+						}
+					}
+
+					if layer.IsRoleException != nil && *layer.IsRoleException != 0 {
+						hasPermission = !userFound
+					} else {
+						hasPermission = userFound
+					}
+
+					if !hasPermission {
+						shouldIncludeLayer = false
+					}
+				}
+			}
+
+			if shouldIncludeLayer {
+				filteredLayers = append(filteredLayers, layer)
+			}
+		}
+
+		if len(filteredLayers) > 0 {
+			currentMap.Categories[i].Layers = filteredLayers
+			filteredCategories = append(filteredCategories, currentMap.Categories[i])
+		}
+	}
+
+	currentMap.Categories = filteredCategories
+
+	for i := range currentMap.Categories {
+		for j := range currentMap.Categories[i].Layers {
+			currentMap.Categories[i].Layers[j].RolePermissions = nil
+			currentMap.Categories[i].Layers[j].UserPermissions = nil
+		}
+	}
+
+	mapStyle, generateErr := generateVectorTileStyle(currentMap.Categories, secure)
+
+	currentMap.Version = mapStyle.Version
+	currentMap.Layers = mapStyle.Layers
+	currentMap.Sources = mapStyle.Sources
+
+	spriteURL := config.LambdaConfig.Domain + "/map/" + id + "/sprite/" + id
+	hasProtocol := strings.HasPrefix(spriteURL, "http://") || strings.HasPrefix(spriteURL, "https://")
+
+	if !hasProtocol {
+		currentMap.Sprite = "https://" + spriteURL
+	} else {
+		currentMap.Sprite = spriteURL
+	}
+
+	if generateErr != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status": "error",
+			"error":  generateErr.Error(),
+		})
 	}
 
 	return c.JSON(currentMap)
