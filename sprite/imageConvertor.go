9bd701649141d0a731575ca7232c629e4716df42
munkh-altai <munkaltai@gmail.com>
2026-03-11 15:58:44 +0800

    marker width 60px

--- sprite/imageConvertor.go
+++ sprite/imageConvertor.go
@@ -28,7 +28,7 @@ func SVGToPNG(svgFile, pngFile string) error {
 	}
 
 	// Set the target size: fixed width of 30px, height scales proportionally
-	targetWidth := 45
+	targetWidth := 60
 	aspectRatio := icon.ViewBox.H / icon.ViewBox.W
 	w := targetWidth
 	h := int(float64(targetWidth) * aspectRatio)
