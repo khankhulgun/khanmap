94038a5c31731fe66a21abae972575c37f144961
munkh-altai <munkaltai@gmail.com>
2026-03-11 15:51:03 +0800

    unique value render added

--- sprite/imageConvertor.go
+++ sprite/imageConvertor.go
@@ -28,7 +28,7 @@ func SVGToPNG(svgFile, pngFile string) error {
 	}
 
 	// Set the target size: fixed width of 30px, height scales proportionally
-	targetWidth := 40
+	targetWidth := 45
 	aspectRatio := icon.ViewBox.H / icon.ViewBox.W
 	w := targetWidth
 	h := int(float64(targetWidth) * aspectRatio)
@@ -44,19 +44,6 @@ func SVGToPNG(svgFile, pngFile string) error {
 
 	// Draw the SVG onto the image
 	icon.Draw(dasher, 1)
-
-	// Remove white background (make white pixels transparent)
-	bounds := img.Bounds()
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			// Check for white pixel (allowing for slight variations/alpha premultiplication)
-			// RGBA() returns color in [0, 65535] range
-			if r > 65000 && g > 65000 && b > 65000 && a > 65000 {
-				img.Set(x, y, image.Transparent)
-			}
-		}
-	}
 
 	// Encode the image as PNG with full transparency
 	out, err := os.Create(pngFile)
